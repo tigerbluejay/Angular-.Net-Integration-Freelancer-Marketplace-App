faa64580d4f633fd5c6bb560b1214cd564a304d1
Jose Maria Iriarte <josemariairiarte@gmail.com>
2025-07-14 16:01:52 -0300

    Scaffolded Angular Client

--- FreelancerApp/API/obj/Debug/net9.0/API.AssemblyInfo.cs
+++ FreelancerApp/API/obj/Debug/net9.0/API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4514bd0b795db7cd75e7a6b176c3faea5e5ebe1a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5b77d0ae4bdbaabe36c44957f2fd3f2815c554d1")]
 [assembly: System.Reflection.AssemblyProductAttribute("API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
