327a23ca00a8e33d30b40b5a29164cd560daf387
Jose Maria Iriarte <josemariairiarte@gmail.com>
2025-07-16 15:52:47 -0300

    Basic http request set up on Angular, Certificates installed to navigate via HTTPS, Print users and their ids on app.component and Debug with Chrome Tools set up in launch.json, other dependencies installed

--- FreelancerApp/client/src/app/app.component.ts
+++ FreelancerApp/client/src/app/app.component.ts
@@ -1,13 +1,34 @@
-import { Component } from '@angular/core';
+import { NgFor } from '@angular/common';
+import { HttpClient } from '@angular/common/http';
+import { Component, inject, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, NgFor],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+  http = inject(HttpClient);
   title = 'FreelancerApp';
+  users: any;
+
+  ngOnInit(): void {
+    setTimeout(() => {
+      console.log('ngOnInit (delayed) running');
+
+      // Add a breakpoint here
+      this.http.get('https://localhost:5001/api/users')
+        .subscribe({
+          next: response => this.users = response, // sets the api response to variable users of type any
+          error: error => console.log(error),
+          complete: () => console.log('Request has completed')
+        })
+
+    }, 1000); // a second delay
+
+    console.log('ngOnInit running')
+  }
 }
